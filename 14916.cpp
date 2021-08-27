9e8f1f38ec94711271713495e7084eddd6197f0c
DESKTOP-9PCDT9N\rjsdn <rjsdntlsjd@gmail.com>
2021-08-27 16:25:22 +0900

    14916 fix

--- 14916.cpp
+++ 14916.cpp
@@ -1,29 +1,63 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
-int main() {
-	int money;
-	cin >> money;
-	int FiveWon = money / 5;
-	int TwoWon = 0;
-	int Target = 5 * FiveWon + 2 * TwoWon;
-	while (Target != money)
+int island[50][50];
+bool visited[50][50];
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { 1, -1, 0, 0 };
+int cnt = 0;
+int w, h;
+
+void bfs(int a, int b)
+{
+	if (visited[a][b])
+		return;
+	queue<pair<int, int>> q;
+	q.push(make_pair(a, b));
+	cnt++;
+	while (!q.empty())
 	{
-		TwoWon++;
-		Target = 5 * FiveWon + 2 * TwoWon;
-		if (Target > money)
+		int x, y;
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		if (island[x][y] == 1)
 		{
-			FiveWon--;
-			TwoWon = 0;
+			for (int i = 0; i < 4; i++)
+			{
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx >= 0 && nx < w)
+					if (ny >= 0 && ny < h)
+					{
+						visited[nx][ny] = true;
+						q.push(make_pair(nx, ny));
+					}
+			}
 		}
-
-		if (FiveWon < 0)
-		{
-			cout << -1;
-			return 0;
-		}
-
+		
 	}
-	cout << FiveWon + TwoWon;
+}
+int main()
+{
+	while (1)
+	{
+		cin >> w >> h;
+		//if (w == 0 && h == 0)
+		//	break;
+		//for (int i = 0; i < h; i++)
+		//{
+		//	for (int j = 0; j < w; j++)
+		//	{
+		//		cin >> island[h][w];
+		//	}
+		//}
+		//for (int i = 0; i < h; i++)
+		//	for (int j = 0; j < w; j++)
+		//		bfs(i, j);
+		//cout << cnt << '\n';
+		//cnt = 0;
+	}
 	return 0;
 }
